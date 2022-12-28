c4d44ba0c8d742fd217040dd2dfce1a1d5613e2a
dheerajkumarjaithuwala <119520595+dheerajkumarjaithuwala@users.noreply.github.com>
2022-12-28 11:20:25 +0530

    Add files via upload

---
+++ PositiveOrNegative.java
@@ -0,0 +1,18 @@
+package flowcontrol;
+
+import java.util.Scanner;
+
+public class PositiveOrNegative {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input number: ");
+		int input = in.nextInt();
+
+		if (input > 0) {
+			System.out.println("Number is positive");
+		} else {
+			System.out.println("Number is negative");
+		}
+	}
+}
